c9aa029def507074b7e72707ce4f5c66c8bcd7a5
oti ayjay <otiedwin40@gmail.com>
2022-12-06 19:47:13 +0100

    add trading view ticker

---
+++ views/t.js
@@ -0,0 +1,24 @@
+[{
+   symbols: [
+      {
+         "proName": "FOREXCOM:SPXUSD",
+         "title": "S&P 500"
+      },
+      {
+         "proName": "FOREXCOM:NSXUSD",
+         "title": "US 100"
+      },
+      {
+         "proName": "FX_IDC:EURUSD",
+         "title": "EUR/USD"
+      },
+      {
+         "proName": "BITSTAMP:BTCUSD",
+         "title": "Bitcoin"
+      },
+      {
+         "proName": "BITSTAMP:ETHUSD",
+         "title": "Ethereum"
+      }
+   ],
+}]
